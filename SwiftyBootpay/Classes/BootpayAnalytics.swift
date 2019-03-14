8609431ebd80e302c9d8f060b5c6dd82756bceec
ehowlsla <jungler@cvdv.me>
2019-03-14 10:57:07 +0900

    Object C Support

--- SwiftyBootpay/Classes/BootpayAnalytics.swift
+++ SwiftyBootpay/Classes/BootpayAnalytics.swift
@@ -80,10 +80,10 @@ public class BootpayAnalytics: NSObject {
         self.iv = getRandomKey(16)
     }
     
-    public static let sharedInstance = BootpayAnalytics()
+    @objc public static let sharedInstance = BootpayAnalytics()
     var application_id = ""
     public var uuid = ""
-    let ver = "2.1.13"
+    let ver = "2.1.14"
     var sk = ""
     var sk_time = 0 // session 유지시간 기본 30분
     var last_time = 0 // 접속 종료 시간
